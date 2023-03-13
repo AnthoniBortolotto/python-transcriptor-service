71a0a0e6a9f6470b0547537573824a8c2e899bbd
AnthoniBortolotto <antho@outlook.com.br>
2023-03-13 15:47:20 -0300

    error handling added

---
+++ README.md
@@ -0,0 +1,10 @@
+### FastAPI youtube videos transcriptor
+===============================
+This is a simple project to transcribe youtube videos using FastAPI and YouTubeTranscriptApi.
+
+## How to run
+1. Clone the repo
+2. Create a virtual environment
+3. Install the requirements
+4. Run the app with uvicorn by running `uvicorn main:app --reload`
+5. Go to http://localhost:8000/{youtube-video-id}?lang={video-transcription-language} to see the docs
--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from mangum import Mangum
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
+handler = Mangum(app)
 
 @app.get("/{id}")
 async def get_subtitles(id: str, lang: str = "en"):
     # importing modules
-    from youtube_transcript_api import YouTubeTranscriptApi
+    try:
+        # using the srt variable with the list of dictionaries
+        # obtained by the .get_transcript() function
+        srt = YouTubeTranscriptApi.get_transcript(id, [lang])
 
-    # using the srt variable with the list of dictionaries
-    # obtained by the .get_transcript() function
-    srt = YouTubeTranscriptApi.get_transcript(id, [lang])
+        # creating or overwriting a file "subtitles.txt" with
+        # the info inside the context manager
+        file = ""
+        # iterating through each element of list srt
+        for i in srt:
+            # writing each element of srt on a new line
+            file += "{}\n".format(i['text'])
 
-    # creating or overwriting a file "subtitles.txt" with
-    # the info inside the context manager
-    file = ""
-    # iterating through each element of list srt
-    for i in srt:
-        # writing each element of srt on a new line
-        file += "{}\n".format(i['text'])
-
-    # return the content of the file as a streaming response
-    return {"transcription": file}
-    #return StreamingResponse(iter([file]), media_type="text/plain")
+        # return the content of the file as a streaming response
+        return {"transcription": file}
+        #return StreamingResponse(iter([file]), media_type="text/plain")
+    except:
+        return {"error": "No transcription found in the language " + lang + " please try another language"} 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
