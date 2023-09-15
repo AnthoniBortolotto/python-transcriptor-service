ef1d50f0f24bc645ce00aa079241f0a8a1ce916c
AnthoniBortolotto <antho@outlook.com.br>
2023-09-15 13:44:04 -0300

    Old comments removed

--- main.py
+++ main.py
@@ -8,22 +8,15 @@ handler = Mangum(app)
 
 @app.get("/{id}")
 async def get_subtitles(id: str, lang: str = "en"):
-    # importing modules
     try:
-        # using the srt variable with the list of dictionaries
-        # obtained by the .get_transcript() function
-        srt = YouTubeTranscriptApi.get_transcript(id, [lang])
-
-        # creating or overwriting a file "subtitles.txt" with
-        # the info inside the context manager
+        srt = YouTubeTranscriptApi.get_transcript(id, [lang]) # getting the subtitles of the video
         file = ""
         # iterating through each element of list srt
         for i in srt:
             # writing each element of srt on a new line
             file += "{} \n ".format(i['text'])
 
-        # return the content of the file as a streaming response
+        # return the content of the file as a json
         return {"transcription": file}
-        # return StreamingResponse(iter([file]), media_type="text/plain")
     except:
         return {"error": "No transcription found in the language " + lang + " please try another language or check the video id"}
